4673ce4213c066e966ea63e5e0af20b899d33a05
Levente181 <Levente181@stud.uni-obuda.hu>
2022-10-14 17:00:52 +0200

    Base Manager Logic created

---
+++ EWYRYV_HFT_2021222.Logic/Classes/ManagerLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EWYRYV_HFT_202223.Repository;
+using EWYRYV_HFT_202223.Models;
+
+namespace EWYRYV_HFT_202223.Logic.Classes
+{
+    class ManagerLogic : IManagerLogic
+    {
+        IRepository<Manager> repo;
+        public ManagerLogic(IRepository<Manager> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Manager item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Manager Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Manager> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Manager item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
---
+++ EWYRYV_HFT_2021222.Logic/Interfaces/IManagerLogic.cs
@@ -0,0 +1,19 @@
+﻿using EWYRYV_HFT_202223.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWYRYV_HFT_202223.Logic
+{
+    interface IManagerLogic
+    {
+        void Create(Manager item);
+        void Delete(int id);
+        Manager Read(int id);
+        IQueryable<Manager> ReadAll();
+        void Update(Manager item);
+    }
+}
+
